479df693ea0cc662e72ab25d368f66b8fbdbfb9b
Casey Key <caseykey@rams.colostate.edu>
2020-02-16 13:47:28 -0700

    fixed service workers error

--- etherradio/src/index.js
+++ etherradio/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
 import { reducer as jPlayers } from 'react-jplayer';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 // Styles the jPlayer to look nice
 import 'react-jplayer/dist/css/skins/sleek.min.css';
@@ -26,4 +24,3 @@ ReactDOM.render((
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
